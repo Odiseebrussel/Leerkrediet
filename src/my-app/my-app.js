1556ea45d402f5e61612c3d8a258e37c4fd87256
Berton <berton.lutinamulamba@student.odisee.be>
2017-11-12 21:45:41 +0100

    Add files via upload

---
+++ src/my-app/my-app.js
@@ -0,0 +1,22 @@
+var myApp = new Framework7();
+var $$ = Dom7;
+ 
+var mainView = myApp.addView('.view-main');
+ 
+myApp.onPageInit('login-screen', function (page) {
+  var pageContainer = $$(page.container);
+  pageContainer.find('.list-button').on('click', function () {
+    var username = pageContainer.find('input[name="username"]').val();
+    var password = pageContainer.find('input[name="password"]').val();
+    // Handle username and password
+     // myApp.alert(`Welcome ${username}`);
+    
+  });
+
+    pageContainer.find('.open-indicator').on('click',function(){
+        myApp.showPreloader();
+      setTimeout(function(){
+          myApp.hidePreloader();;
+      },2000);
+    });
+});     
