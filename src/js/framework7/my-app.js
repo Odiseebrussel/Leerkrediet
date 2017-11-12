5d34e6e50a4f5270976b2359d20f033dae18a7f6
Berton <berton.lutinamulamba@student.odisee.be>
2017-11-12 21:48:15 +0100

    Add files via upload

---
+++ src/js/framework7/my-app.js
@@ -0,0 +1,15 @@
+"use strict";
+/*global Framework7, Dom7 */
+var myApp = new Framework7();
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    dynamicNavbar: true
+});
+
+
+/* databron :
+        http://ophalvens.net/mi2/examen/alerts.php?m=all
+ */
+
